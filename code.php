25b52619bb34d962a86ef2a83ee852589298a7d2
Mirmuxsin <58307677+Mirmuxsin@users.noreply.github.com>
2020-05-02 00:41:22 +0500

    Uploaded code.php

--- code.php
+++ code.php
@@ -1,12 +1,12 @@
 <?php
 if($msg == "/start"){
 
-	sm($userid, "Hello world, $name", $key);
 	$key = json_encode([
 		'inline_keyboard'=>[
 			[['text'=>'button 1', 'callback_data' => "1"], ['text'=> "button 2", 'callback_data'=> "2"]],
 			[['text'=>'button 3', 'callback_data' => "3"], ['text'=> "button 4", 'callback_data'=> "4"]],
 		],
 	]);
+	sm($userid, "Hello world, $name", $key);
 
 }
